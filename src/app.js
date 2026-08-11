0b0867695e86f0abde277a5f88d054fd95f8d954
Hediyeh Afzalzdeh <hedye.afzalzade@gmail.com>
2026-08-11 13:37:42 +0200

    feat: implement user login

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 
 export default app;
